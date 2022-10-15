68124834cab2ec1593a97392de77494cd6843803
Harry <vin-ig@yandex.ru>
2022-10-15 20:59:04 +0300

    password repeat corrected

--- core/serializers.py
+++ core/serializers.py
@@ -1,20 +1,36 @@
-from abc import ABC
-
 from django.contrib.auth.password_validation import validate_password
-from djoser.serializers import UserCreateSerializer as BaseUserRegistrationSerializer, UserSerializer, \
-	UserCreatePasswordRetypeSerializer
+from djoser.serializers import UserSerializer, UserCreatePasswordRetypeSerializer, UserCreateSerializer
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-from rest_framework_simplejwt.serializers import PasswordField
 
 User = get_user_model()
 
 
-# class UserRegistrationSerializer(BaseUserRegistrationSerializer):
 class UserRegistrationSerializer(UserCreatePasswordRetypeSerializer):
 	class Meta:
 		model = User
-		fields = '__all__'
+		fields = [
+			'id',
+			'username',
+			'email',
+			'first_name',
+			'last_name',
+			'password'
+		]
+
+	def __init__(self, *args, **kwargs):
+		super(UserCreateSerializer, self).__init__(*args, **kwargs)
+		self.fields["password_repeat"] = serializers.CharField(
+			style={"input_type": "password"}
+		)
+
+	def validate(self, attrs):
+		self.fields.pop("password_repeat", None)
+		password_repeat = attrs.pop("password_repeat")
+		if attrs["password"] == password_repeat:
+			return attrs
+		else:
+			self.fail("password_mismatch")
 
 	def create(self, validated_data):
 		user = User.objects.create(**validated_data)
