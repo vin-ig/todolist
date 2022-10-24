c658066a30f9f6250e28a9719c2beb1fff7acbd4
Harry <vin-ig@yandex.ru>
2022-10-24 17:35:23 +0300

    Models for Board and BoardParticipant created

--- goals/models.py
+++ goals/models.py
@@ -32,6 +32,43 @@ class Priority(models.IntegerChoices):
 	critical = 4, "Критический"
 
 
+class Board(DatesModelMixin):
+	class Meta:
+		verbose_name = "Доска"
+		verbose_name_plural = "Доски"
+
+	title = models.CharField(verbose_name="Название", max_length=255)
+	is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
+class BoardParticipant(DatesModelMixin):
+	class Meta:
+		unique_together = ("board", "user")
+		verbose_name = "Участник"
+		verbose_name_plural = "Участники"
+
+	class Role(models.IntegerChoices):
+		owner = 1, "Владелец"
+		writer = 2, "Редактор"
+		reader = 3, "Читатель"
+
+	board = models.ForeignKey(
+		Board,
+		verbose_name="Доска",
+		on_delete=models.PROTECT,
+		related_name="participants",
+	)
+	user = models.ForeignKey(
+		User,
+		verbose_name="Пользователь",
+		on_delete=models.PROTECT,
+		related_name="participants",
+	)
+	role = models.PositiveSmallIntegerField(
+		verbose_name="Роль", choices=Role.choices, default=Role.owner
+	)
+
+
 class GoalCategory(DatesModelMixin):
 	class Meta:
 		verbose_name = "Категория"
@@ -40,6 +77,9 @@ class GoalCategory(DatesModelMixin):
 	title = models.CharField(verbose_name="Название", max_length=255)
 	user = models.ForeignKey(User, verbose_name="Автор", on_delete=models.PROTECT)
 	is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+	board = models.ForeignKey(
+		Board, verbose_name="Доска", on_delete=models.PROTECT, related_name="categories"
+	)
 
 
 class Goal(DatesModelMixin):
