f471d8621eb000f783e3ef57757b511370001129
Harry <vin-ig@yandex.ru>
2022-10-31 21:49:06 +0300

    Dataclasses completed

---
+++ bot/tg/client.py
@@ -0,0 +1,17 @@
+import requests
+
+from bot.tg.dc import GetUpdatesResponse, SendMessageResponse
+
+
+class TgClient:
+    def __init__(self, token):
+        self.token = token
+
+    def get_url(self, method: str):
+        return f"https://api.telegram.org/bot{self.token}/{method}"
+
+    def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
+        raise NotImplementedError
+
+    def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
+        raise NotImplementedError
--- bot/tg/dc.py
+++ bot/tg/dc.py
@@ -1,24 +1,32 @@
 from dataclasses import dataclass
 from typing import List
 from marshmallow import EXCLUDE
+from datetime import datetime
 
 
 @dataclass
-class UpdateObj:
-	pass
+class MessageFrom:
+	id: int
+	is_bot: bool
+	first_name: str
+	username: str
+	language_code: str
 
 
 @dataclass
-class GetUpdatesResponse:
-	ok: bool
-	result: List[UpdateObj]
-
-	class Meta:
-		unknown = EXCLUDE
+class Chat:
+	id: int
+	first_name: str
+	username: str
+	type: str
 
 
 @dataclass
 class Message:
+	message_id: int
+	message_from: MessageFrom
+	chat: Chat
+	date: datetime.timestamp
 	text: str
 
 
@@ -26,6 +34,21 @@ class Message:
 class SendMessageResponse:
 	ok: bool
 	result: Message
+
+	class Meta:
+		unknown = EXCLUDE
+
+
+@dataclass
+class UpdateObj:
+	update_id: int
+	message: Message
+
+
+@dataclass
+class GetUpdatesResponse:
+	ok: bool
+	result: List[UpdateObj]
 
 	class Meta:
 		unknown = EXCLUDE
