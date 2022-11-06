8adbdaedc97db24ff94ec90c5108576e98caed43
Harry <vin-ig@yandex.ru>
2022-11-06 19:08:35 +0300

    Crate a new user in tg_bog fixed

--- bot/models.py
+++ bot/models.py
@@ -9,6 +9,6 @@ class TgUser(models.Model):
 		verbose_name_plural = 'Пользователи Telegram'
 
 	tg_id = models.IntegerField(verbose_name='ID чата')
-	username = models.CharField(max_length=50, verbose_name='Username пользователя в tg')
+	username = models.CharField(max_length=50, verbose_name='Username пользователя в tg', null=True)
 	user = models.ForeignKey(User, verbose_name='Пользователь', on_delete=models.PROTECT, null=True)
 	verification_code = models.CharField(max_length=150, verbose_name='Код верификации')
